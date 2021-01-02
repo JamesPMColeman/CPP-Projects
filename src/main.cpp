c060581974e157cc7192bab68bd2f70c9d3455ec
James Coleman <jcolem22@msudenver.edu>
2021-01-02 13:41:47 -0700

    Initial configuration
    Configuration for the main project and tests is set up. Program
    can be executed.

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+
+	std::cout << "\n Hello World. \n";
+
+	return 0;
+}
---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+	
+	std::cout << "\n Here are the test results. \n";
+
+	return 0;
+}
